aec759ea495811afe7696259f88213ed03e8496e
Domogo <dsatrapaa@gmail.com>
2018-12-31 20:35:48 +0100

    eh

--- adviceSite/settings.py
+++ adviceSite/settings.py
@@ -73,7 +73,7 @@ WSGI_APPLICATION = 'adviceSite.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-
+"""
 if 'RDS_HOSTNAME' in os.environ:
     DATABASES = {
         'default': {
@@ -86,12 +86,13 @@ if 'RDS_HOSTNAME' in os.environ:
         }
     }
 else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
+"""
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+}
 
 
 # Password validation
